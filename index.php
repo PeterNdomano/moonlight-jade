42b87d2b4d0bb8d03410f15a3442141f521579c1
PeterNdomano <peterndomano@gmail.com>
2022-04-10 14:36:41 +0300

    commit #332

--- index.php
+++ index.php
@@ -5,12 +5,65 @@
         <title>Mock up</title>
         <link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/assets/libs/bootstrap/css/bootstrap.min.css"></link>
         <link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/assets/libs/mdb/css/mdb.min.css"></link>
+        <link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/style.css"></link>
+
         <script src="<?php echo get_template_directory_uri() ?>/assets/libs/jquery-3.2.1.min.js"></script>
         <script src="<?php echo get_template_directory_uri() ?>/assets/libs/popper.min.js"></script>
         <script src="<?php echo get_template_directory_uri() ?>/assets/libs/bootstrap/js/bootstrap.min.js"></script>
         <script src="<?php echo get_template_directory_uri() ?>/assets/libs/mdb/js/mdb.min.js"></script>
     </head>
     <body>
-        
+        <header>
+            <nav class="mn-topbar mn-topbar-fixed">
+                <div class="d-flex mn-full-height container">
+                    <div class="mn-menu-btn align-self-center p-2">
+                        <i class="icon-menu"></i>
+                    </div>
+                    <div class="mn-topbar-title align-self-center p-1">
+                        <h3>Site Title</h3>
+                    </div>
+                    <div class="mn-nav-items-container-lg flex-grow-1 mn-full-height d-flex justify-content-end">
+                        <div class="mn-nav-item align-self-center" data-active="true">
+                            <h3>Home</h3>
+                        </div>
+                        <div class="mn-nav-item align-self-center">
+                            <h3>Blog</h3>
+                        </div>
+                        <div class="mn-nav-item align-self-center">
+                            <h3>Products</h3>
+                        </div>
+                        <div class="mn-nav-item align-self-center">
+                            <h3>Services</h3>
+                        </div>
+                        <div class="mn-nav-item align-self-center">
+                            <h3>About</h3>
+                        </div>
+                        <div class="mn-nav-item align-self-center">
+                            <h3>Contact</h3>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+            <aside class="mn-nav-items-container-sm">
+                <div class="mn-nav-item" data-active="true">
+                    <h3>Home</h3>
+                </div>
+                <div class="mn-nav-item">
+                    <h3>Blog</h3>
+                </div>
+                <div class="mn-nav-item">
+                    <h3>Products</h3>
+                </div>
+                <div class="mn-nav-item">
+                    <h3>Services</h3>
+                </div>
+                <div class="mn-nav-item">
+                    <h3>About</h3>
+                </div>
+                <div class="mn-nav-item">
+                    <h3>Contact</h3>
+                </div>
+            </aside>
+        </header>
     </body>
 </html>
