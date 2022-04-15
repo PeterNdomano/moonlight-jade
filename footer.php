305b4055dc09cb5c69a1b71bc986b5c2b11ba7b7
PeterNdomano <peterndomano@gmail.com>
2022-04-16 00:54:40 +0300

    created footer.php

---
+++ footer.php
@@ -0,0 +1,57 @@
+<footer class="mn-footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <h3 class="mn-footer-info-title">Contacts</h3>
+                <p>
+                    <i class="icon-phone mn-social-icon"></i>
+                    +255 767 XXX XXX
+                </p>
+                <p>
+                    <i class="icon-phone mn-social-icon"></i>
+                    +254 767 XXX XXX
+                </p>
+                <p>
+                    <i class="icon-envelope-letter mn-social-icon"></i>
+                    support@site.com
+                </p>
+                <p>
+                    <i class="icon-envelope-letter mn-social-icon"></i>
+                    admin@site.com
+                </p>
+            </div>
+
+            <div class="col-md-4">
+                <h3 class="mn-footer-info-title">Main Office</h3>
+                <p>Lindi Tanzania</p>
+                <p>P.O.Box 236</p>
+            </div>
+
+            <div class="col-md-4">
+                <h3 class="mn-footer-info-title">Social Media</h3>
+                <p>
+                    <i class="icon-social-facebook mn-social-icon"></i>
+                    <a href="#">Facebook</a>
+                </p>
+                <p>
+                    <i class="icon-social-instagram mn-social-icon"></i>
+                    <a href="#">Instagram</a>
+                </p>
+                <p>
+                    <i class="icon-social-twitter mn-social-icon"></i>
+                    <a href="#">Twitter</a>
+                </p>
+            </div>
+
+            <div class="col-md-12 text-left mn-footer-credit">
+                <hr />
+                <h6><a href="#">Theme By Peter Ndomano</a></h6>
+                <h6><a href="#">Powered By WordPress</a></h6>
+                <h6>&copy;siteowner</h6>
+            </div>
+        </div>
+    </div>
+</footer>
+<?php wp_footer() ?>
+</body>
+</html>
