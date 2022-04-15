8454c9276a3999bba77c506ce739fb53276584ac
PeterNdomano <peterndomano@gmail.com>
2022-04-16 01:01:28 +0300

    created section-blog

--- index.php
+++ index.php
@@ -3,5 +3,6 @@ get_header();
 get_template_part('section-products');
 get_template_part('section-services');
 get_template_part('section-reviews');
+get_template_part('section-blog');
 get_footer();
 ?>
---
+++ section-blog.php
@@ -0,0 +1,40 @@
+<section class="mn-section">
+    <div class="container">
+        <h3 class="mn-section-title">
+            <i class="mdi mdi-view-dashboard mn-section-icon"></i>
+            Blog
+        </h3>
+
+        <div class="row">
+            <!--2 most recent blog posts-->
+            <div class="col-md-8">
+                <div class="card mn-item-card" style="background-image: url(<?php echo get_template_directory_uri() ?>/assets/images/img1.jpg)">
+                    <div class="card-img-overlay">
+                        <div>
+                            <h4 class="card-title">Post 1 title</h4>
+                            <p style="card-text">Post 1 excerp comes here to introduce reader to this post</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4">
+                <div class="card mn-item-card" style="background-image: url(<?php echo get_template_directory_uri() ?>/assets/images/bg1.jpg)">
+                    <div class="card-img-overlay">
+                        <div>
+                            <h4 class="card-title">Post 2 title</h4>
+                            <p style="card-text">Post 2 excerp comes here to introduce reader to this post</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-12 text-right">
+                <a class="btn mn-btn-next text-dark" href="#">
+                    More Blog Posts
+                    <i class="icon-arrow-right-circle"></i>
+                </a>
+            </div>
+        </div>
+    </div>
+</section>
