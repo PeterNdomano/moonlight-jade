45416830460c6eb99417e9aaa215da0f5687422b
PeterNdomano <peterndomano@gmail.com>
2022-04-16 01:00:06 +0300

    created section services

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 get_header();
 get_template_part('section-products');
+get_template_part('section-services');
 get_template_part('section-reviews');
 get_footer();
 ?>
---
+++ section-services.php
@@ -0,0 +1,51 @@
+<section class="mn-section">
+    <div class="container">
+        <h3 class="mn-section-title">
+            <i class="mdi mdi-view-dashboard mn-section-icon"></i>
+            Services
+        </h3>
+
+        <div class="row">
+            <!--3 most recent services-->
+            <div class="col-md-3">
+                <div class="card mn-item-card" style="background-image: url(<?php echo get_template_directory_uri() ?>/assets/images/img1.jpg)">
+                    <div class="card-img-overlay">
+                        <div>
+                            <h4 class="card-title">Service 1</h4>
+                            <p style="card-text">Service short description comes here hhhh hhhh</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-3">
+                <div class="card mn-item-card" style="background-image: url(<?php echo get_template_directory_uri() ?>/assets/images/bg1.jpg)">
+                    <div class="card-img-overlay">
+                        <div>
+                            <h4 class="card-title">Service 2</h4>
+                            <p style="card-text">Service short description comes here hhhh hhhh</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-6">
+                <div class="card mn-item-card" style="background-image: url(<?php echo get_template_directory_uri() ?>/assets/images/img1.jpg)">
+                    <div class="card-img-overlay">
+                        <div>
+                            <h4 class="card-title">Service 3</h4>
+                            <p style="card-text">Service short description comes here hhhh hhhh</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-12 text-right">
+                <a class="btn mn-btn-next text-dark" href="#">
+                    More Services
+                    <i class="icon-arrow-right-circle"></i>
+                </a>
+            </div>
+        </div>
+    </div>
+</section>
