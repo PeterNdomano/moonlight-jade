7c1b76bb8effe54beb81ed045c2528e891ede435
PeterNdomano <peterndomano@gmail.com>
2022-04-16 18:32:37 +0300

    shortener filter,

--- functions.php
+++ functions.php
@@ -1,11 +1,17 @@
 <?php
+add_action( 'init', 'mn_create_posttypes' );
+add_filter( 'the_excerpt', 'mn_excerpt_shortener');
 
-//adding functionality support
-if (function_exists('add_theme_support')) {
-    add_theme_support('menus');
+function mn_excerpt_shortener( $content ){
+
 }
 
 function mn_create_posttypes() {
+    add_theme_support('post-thumbnails');
+    add_post_type_support( 'products', 'thumbnail' );
+    add_post_type_support( 'services', 'thumbnail' );
+    add_post_type_support( 'reviews', 'thumbnail' );
+
     //products
     register_post_type( 'products',
         array(
@@ -13,6 +19,7 @@ function mn_create_posttypes() {
                 'name' => __( 'Products' ),
                 'singular_name' => __( 'Product' )
             ),
+            'supports' => array( 'title', 'editor', 'excerpt', 'author', 'thumbnail', 'comments', 'revisions', 'custom-fields',),
             'public' => true,
             'has_archive' => true,
             'rewrite' => array('slug' => 'products'),
@@ -28,6 +35,7 @@ function mn_create_posttypes() {
                 'name' => __( 'Services' ),
                 'singular_name' => __( 'Service' )
             ),
+            'supports' => array( 'title', 'editor', 'excerpt', 'author', 'thumbnail', 'comments', 'revisions', 'custom-fields',),
             'public' => true,
             'has_archive' => true,
             'rewrite' => array('slug' => 'services'),
@@ -43,6 +51,7 @@ function mn_create_posttypes() {
                 'name' => __( 'Reviews' ),
                 'singular_name' => __( 'Review' )
             ),
+            'supports' => array( 'title', 'editor', 'excerpt', 'author', 'thumbnail', 'comments', 'revisions', 'custom-fields',),
             'public' => true,
             'has_archive' => true,
             'rewrite' => array('slug' => 'reviews'),
@@ -52,5 +61,4 @@ function mn_create_posttypes() {
     );
 }
 
-add_action( 'init', 'mn_create_posttypes' );
 ?>
--- sections/section-products.php
+++ sections/section-products.php
@@ -1,5 +1,9 @@
 <?php
-    
+$args = array(
+    'post_type' => 'products',
+    'post_per_page' => 3,
+);
+$the_query = new WP_Query( $args );
 ?>
 <section class="mn-section">
     <div class="container">
@@ -10,6 +14,38 @@
 
         <div class="row">
             <!--3 most recent products-->
+            <?php if ( $the_query->have_posts() ): ?>
+            <?php $count = 1; ?>
+            <?php while( $the_query->have_posts()) : $the_query->the_post(); ?>
+            <?php
+                $thumb_id = get_post_thumbnail_id();
+                $thumb_url_array = wp_get_attachment_image_src($thumb_id, 'thumbnail-size', true);
+                $thumb_url = $thumb_url_array[0];
+                if($count === 1){
+                    ?>
+                    <div class="col-md-6">
+                        <div class="card mn-item-card" style="background-image: url(<?php echo $thumb_url ?>">
+                            <div class="card-img-overlay">
+                                <div>
+                                    <h4 class="card-title"><?php the_title() ?></h4>
+                                    <p style="card-text"><?php the_excerpt() ?></p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <?php
+                }
+                else{
+                    ?>
+
+                    <?php
+                }
+            ?>
+            <?php $count++ ?>
+        <?php endwhile; wp_reset_postdata(); ?>
+            <?php else: ?>
+                <p><?php _e( 'Sorry, no products were found' ); ?></p>
+            <?php endif; ?>
             <div class="col-md-6">
                 <div class="card mn-item-card" style="background-image: url(<?php echo get_template_directory_uri() ?>/assets/images/img1.jpg)">
                     <div class="card-img-overlay">
