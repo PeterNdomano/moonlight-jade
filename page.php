f4c50103b5056d6c66288d70e5952ba146c70ca0
PeterNdomano <peterndomano@gmail.com>
2022-04-16 21:19:42 +0300

    closes #3

--- page.php
+++ page.php
@@ -1,15 +1,54 @@
 <?php
 get_header();
-
+$thumb_id = get_post_thumbnail_id();
+$thumb_url_array = wp_get_attachment_image_src($thumb_id, 'thumbnail-size', true);
+$thumb_url = "";
+if(has_post_thumbnail()){
+    $thumb_url = $thumb_url_array[0];
+}
 ?>
 
 <section class="mn-section">
-    <div class="container">
+    <?php
+    if(has_post_thumbnail()){
+        ?>
+        <div class="card mn-post-img" style="background-image:url(<?php echo $thumb_url ?>)">
+            <div class="card-img-overlay">
+                <div class="container text-center">
+                    <h1 class="mn-h1 text-light"><b><?php the_title() ?></b></h1>
+                    <div class="mn-meta-container">
+                        <h6>
+                            <i>On <?php echo get_the_date() ?></i>
+                        </h6>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <?php
+    }
+    ?>
+    <div class="container" style="padding-top: var(--topBarHeight);padding-bottom: var(--topBarHeight);">
         <div class="row">
-            <div class="col-md-4">
-                <h3 class="mn-section-title" style="width:100%;">
-                    <?php echo the_title() ?>
-                </h3>
+            <div class="col-md-4 p-3">
+                <?php
+                if(has_post_thumbnail()){
+                    ?>
+                    <h4 class="mn-section-title" style="width:100%;">
+                        "<i><?php the_excerpt() ?></i>"
+                    </h4>
+                    <?php
+                }
+                else{
+                    ?>
+                    <h3 class="mn-section-title" style="width:100%;">
+                        <?php the_title() ?>
+                    </h3>
+                    <h6>
+                        <b><i>On <?php echo get_the_date() ?></i></b>
+                    </h6>
+                    <?php
+                }
+                ?>
             </div>
             <div class="col-md-8 p-2">
                 <?php echo the_content() ?>
